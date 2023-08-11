2f2c596956f04a0446a124256dc654c514570cd4
Dorde Zivanovic <djordjezivanovicva@gmail.com>
2023-08-11 00:14:21 +0000

    1. Constants

--- main.cu
+++ main.cu
@@ -4,17 +4,17 @@
 // dimx is a number of columns
 // dimy is a number of rows
 bool checkResults (float *gold, float *d_data, int dimx, int dimy, float rel_tol) {
-	for (int iy = 0; iy < dimy; ++iy) 
-  {
-		for (int ix = 0; ix < dimx; ++ix) 
-    {
+	for (int iy = 0; iy < dimy; ++iy)
+	{
+		for (int ix = 0; ix < dimx; ++ix)
+		{
 			int idx = iy * dimx + ix;
 
 			float gdata = gold[idx];
 			float ddata = d_data[idx];
 
-			if (isnan(gdata) || isnan(ddata)) 
-      {
+			if (isnan(gdata) || isnan(ddata))
+			{
 				printf("Nan detected: gold %f, device %f\n", gdata, ddata);
 				return false;
 			}
@@ -25,8 +25,8 @@ bool checkResults (float *gold, float *d_data, int dimx, int dimy, float rel_tol
 			else
 				rdiff = fabs(gdata - ddata) / fabs(gdata);
 
-			if (rdiff > rel_tol) 
-      {
+			if (rdiff > rel_tol)
+			{
 				printf("Error solutions don't match at iy=%d, ix=%d.\n", iy, ix);
 				printf("gold: %f, device: %f\n", gdata, ddata);
 				printf("rdiff: %f\n", rdiff);
@@ -37,33 +37,33 @@ bool checkResults (float *gold, float *d_data, int dimx, int dimy, float rel_tol
 	return true;
 }
 
-void computeCpuResults(float *g_data, int dimx, int dimy, int niterations, int nreps) 
+void computeCpuResults(float *g_data, int dimx, int dimy, int niterations, int nreps)
 {
-	for (int r = 0; r < nreps; r++) 
-  {
+	for (int r = 0; r < nreps; r++)
+	{
 		printf("Rep: %d\n", r);
 #pragma omp parallel for
-		for (int iy = 0; iy < dimy; ++iy) 
-    {
+		for (int iy = 0; iy < dimy; ++iy)
+		{
 			for (int ix = 0; ix < dimx; ++ix)
-      {
+			{
 				int idx = iy * dimx + ix;
 
 				float value = g_data[idx];
 
-				for (int i = 0; i < niterations; i++) 
-        {
-					if (ix % 4 == 0) 
-          {
+				for (int i = 0; i < niterations; i++)
+				{
+					if (ix % 4 == 0)
+					{
 						value += sqrtf(logf(value) + 1.f);
-					} else if (ix % 4 == 1) 
-          {
+					} else if (ix % 4 == 1)
+					{
 						value += sqrtf(cosf(value) + 1.f);
-					} else if (ix % 4 == 2) 
-          {
+					} else if (ix % 4 == 2)
+					{
 						value += sqrtf(sinf(value) + 1.f);
-					} else if (ix % 4 == 3) 
-          {
+					} else if (ix % 4 == 3)
+					{
 						value += sqrtf(tanf(value) + 1.f);
 					}
 				}
@@ -73,101 +73,101 @@ void computeCpuResults(float *g_data, int dimx, int dimy, int niterations, int n
 	}
 }
 
-__global__ void kernel_A(float *g_data, int dimx, int dimy, int niterations) 
+__global__ void kernel_A(float *g_data, const int dimx, const int dimy, const int niterations)
 {
-  //printf("blockDim.y %d blockIdx.y %d threadIdx.y %d \n", blockDim.y, blockIdx.y,  threadIdx.y);
-  //printf("blockDim.x %d blockIdx.y %d threadIdx.x %d \n", blockDim.x, blockIdx.x, threadIdx.x);
-  int idx; 
-  float value;
-  for (int ix = blockIdx.x * blockDim.x + threadIdx.x; ix < dimx; ix += blockDim.x * gridDim.x)
-  {
-    if (ix % 4 == 0)
-    {
-      for (int iy = blockIdx.y * blockDim.y + threadIdx.y; iy < dimy; iy += blockDim.y * gridDim.y)
-      {
-        idx = iy * dimx + ix;
-        value = g_data[idx];
+	//printf("blockDim.y %d blockIdx.y %d threadIdx.y %d \n", blockDim.y, blockIdx.y, threadIdx.y);
+	//printf("blockDim.x %d blockIdx.y %d threadIdx.x %d \n", blockDim.x, blockIdx.x, threadIdx.x);
+	int idx;
+	float value;
+	for (int ix = blockIdx.x * blockDim.x + threadIdx.x; ix < dimx; ix += blockDim.x * gridDim.x)
+	{
+		if (ix % 4 == 0)
+		{
+			for (int iy = blockIdx.y * blockDim.y + threadIdx.y; iy < dimy; iy += blockDim.y * gridDim.y)
+			{
+				idx = iy * dimx + ix;
+				value = g_data[idx];
 
-        for (int i = 0; i < niterations; i++)
-        {
-            value += sqrtf(logf(value) + 1.f);
-        }
-        g_data[idx] = value;
-      }
-    }
-    else if (ix % 4 == 1)
-    {
-      for (int iy = blockIdx.y * blockDim.y + threadIdx.y; iy < dimy; iy += blockDim.y * gridDim.y)
-      {
-        idx = iy * dimx + ix;
-        value = g_data[idx];
-
-        for (int i = 0; i < niterations; i++)
-        {
-            value += sqrtf(cosf(value) + 1.f);
-        }
-        g_data[idx] = value;
-      }
-    }
-    else if (ix % 4 == 2)
-    {
-      for (int iy = blockIdx.y * blockDim.y + threadIdx.y; iy < dimy; iy += blockDim.y * gridDim.y)
-      {
-        idx = iy * dimx + ix;
-        value = g_data[idx];
-
-        for (int i = 0; i < niterations; i++)
-        {
-          value += sqrtf(sinf(value) + 1.f);
-        }
-        g_data[idx] = value;
-      }
-    }
-    else if (ix %  4 == 3)
-    {
-      for (int iy = blockIdx.y * blockDim.y + threadIdx.y; iy < dimy; iy += blockDim.y * gridDim.y)
-      {
-        idx = iy * dimx + ix;
-        value = g_data[idx];
-
-        for (int i = 0; i < niterations; i++)
-        {
-            value += sqrtf(tanf(value) + 1.f);
-        }
-        g_data[idx] = value;
-      }
-    }
-    /*
-    for (int iy = blockIdx.y * blockDim.y + threadIdx.y; iy < dimy; iy += blockDim.y * gridDim.y) 
-    {
-			int idx = iy * dimx + ix;
-      //printf("%d\n", idx);
-			float value = g_data[idx];
-
-			for (int i = 0; i < niterations; i++) 
-      {
-				if (ix % 4 == 0) 
-        {
+				for (int i = 0; i < niterations; i++)
+				{
 					value += sqrtf(logf(value) + 1.f);
-				} else if (ix % 4 == 1) 
-        {
+				}
+				g_data[idx] = value;
+			}
+		}
+		else if (ix % 4 == 1)
+		{
+			for (int iy = blockIdx.y * blockDim.y + threadIdx.y; iy < dimy; iy += blockDim.y * gridDim.y)
+			{
+				idx = iy * dimx + ix;
+				value = g_data[idx];
+
+				for (int i = 0; i < niterations; i++)
+				{
 					value += sqrtf(cosf(value) + 1.f);
-				} else if (ix % 4 == 2) 
-        {
+				}
+				g_data[idx] = value;
+			}
+		}
+		else if (ix % 4 == 2)
+		{
+			for (int iy = blockIdx.y * blockDim.y + threadIdx.y; iy < dimy; iy += blockDim.y * gridDim.y)
+			{
+				idx = iy * dimx + ix;
+				value = g_data[idx];
+
+				for (int i = 0; i < niterations; i++)
+				{
 					value += sqrtf(sinf(value) + 1.f);
-				} else if (ix % 4 == 3) 
-        {
+				}
+				g_data[idx] = value;
+			}
+		}
+		else if (ix % 4 == 3)
+		{
+			for (int iy = blockIdx.y * blockDim.y + threadIdx.y; iy < dimy; iy += blockDim.y * gridDim.y)
+			{
+				idx = iy * dimx + ix;
+				value = g_data[idx];
+
+				for (int i = 0; i < niterations; i++)
+				{
 					value += sqrtf(tanf(value) + 1.f);
 				}
+				g_data[idx] = value;
 			}
-			g_data[idx] = value;
 		}
-    */
+		/*
+		   for (int iy = blockIdx.y * blockDim.y + threadIdx.y; iy < dimy; iy += blockDim.y * gridDim.y)
+		   {
+		   int idx = iy * dimx + ix;
+		   //printf("%d\n", idx);
+		   float value = g_data[idx];
+
+		   for (int i = 0; i < niterations; i++)
+		   {
+		   if (ix % 4 == 0)
+		   {
+		   value += sqrtf(logf(value) + 1.f);
+		   } else if (ix % 4 == 1)
+		   {
+		   value += sqrtf(cosf(value) + 1.f);
+		   } else if (ix % 4 == 2)
+		   {
+		   value += sqrtf(sinf(value) + 1.f);
+		   } else if (ix % 4 == 3)
+		   {
+		   value += sqrtf(tanf(value) + 1.f);
+		   }
+		   }
+		   g_data[idx] = value;
+		   }
+		   */
 	}
 }
 
 
-void launchKernel(float * d_data, int dimx, int dimy, int niterations) 
+void launchKernel(float * d_data, int dimx, int dimy, int niterations)
 {
 	// Only change the contents of this function and the kernel(s). You may
 	// change the kernel's function signature as you see fit.
@@ -177,12 +177,12 @@ void launchKernel(float * d_data, int dimx, int dimy, int niterations)
 	cudaGetDeviceProperties(&prop, 0);
 	int num_sms = prop.multiProcessorCount;
 
-	dim3 block(4, 128);
+	dim3 block(1, 128);
 	dim3 grid(1, num_sms);
 	kernel_A<<<grid, block>>>(d_data, dimx, dimy, niterations);
 }
 
-float timing_experiment(float *d_data, int dimx, int dimy, int niterations, int nreps) 
+float timing_experiment(float *d_data, int dimx, int dimy, int niterations, int nreps)
 {
 	float elapsed_time_ms = 0.0f;
 	cudaEvent_t start, stop;
@@ -190,8 +190,8 @@ float timing_experiment(float *d_data, int dimx, int dimy, int niterations, int 
 	cudaEventCreate(&stop);
 
 	cudaEventRecord(start, 0);
-	for (int i = 0; i < nreps; i++) 
-  {
+	for (int i = 0; i < nreps; i++)
+	{
 		launchKernel(d_data, dimx, dimy, niterations);
 	}
 	cudaEventRecord(stop, 0);
@@ -217,25 +217,25 @@ int main() {
 	float *d_data = 0, *h_data = 0, *h_gold = 0;
 	cudaMalloc((void **)&d_data, nbytes);
 	if (0 == d_data)
-  {
+	{
 		printf("couldn't allocate GPU memory\n");
 		return -1;
 	}
 	printf("allocated %.2f MB on GPU\n", nbytes / (1024.f * 1024.f));
 	h_data = (float *)malloc(nbytes);
 	h_gold = (float *)malloc(nbytes);
-	if (0 == h_data || 0 == h_gold) 
-  {
+	if (0 == h_data || 0 == h_gold)
+	{
 		printf("couldn't allocate CPU memory\n");
 		return -2;
 	}
 	printf("allocated %.2f MB on CPU\n", 2.0f * nbytes / (1024.f * 1024.f));
 	for (int i = 0; i < dimx * dimy; i++)
-    h_gold[i] = 1.0f + 0.01 * (float)rand() / (float)RAND_MAX;
+		h_gold[i] = 1.0f + 0.01 * (float)rand() / (float)RAND_MAX;
 	cudaMemcpy(d_data, h_gold, nbytes, cudaMemcpyHostToDevice);
 
 	float runtime_cuda = timing_experiment(d_data, dimx, dimy, niterations, 1);
-  printf("Cuda runtime %.3f\n", runtime_cuda);
+	printf("Cuda runtime %.3f\n", runtime_cuda);
 	printf("Verifying solution\n");
 
 	cudaMemcpy(h_data, d_data, nbytes, cudaMemcpyDeviceToHost);
@@ -245,12 +245,12 @@ int main() {
 
 	bool pass = checkResults(h_gold, h_data, dimx, dimy, rel_tol);
 
-	if (pass) 
-  {
+	if (pass)
+	{
 		printf("Results are correct\n");
-	} 
-  else 
-  {
+	}
+	else
+	{
 		printf("FAIL: results are incorrect\n");
 	}
 
